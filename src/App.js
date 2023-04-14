e4611133007e01dbfa37224d883495f35de8aa32
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2023-04-14 15:48:52 +0200

    First Docker project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          J'aime bien les pommes de terre
         </a>
       </header>
     </div>
